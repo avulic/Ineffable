9577b2819192319d3aabc017cf53eaecd466c777
avulic <antevulic96@gmail.com>
2018-06-23 22:13:08 +0200

    dodana registracijja

---
+++ Ineffable/PrijavaRegistracija/Autentifikator.cs
@@ -0,0 +1,66 @@
+﻿using dbModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrijavaRegistracija
+{
+    class Autentifikator
+    {
+        public bool registrirajKorisnika(string ime, string prezime, string email, string korIme, string lozinka, string adresa, string telefon)
+        {
+            bool ispravno = false;
+            using (IneffableEntities bp = new IneffableEntities())
+            {
+                foreach (var item in bp.korisniks)
+                {
+                    if (item.korisnicko_ime == korIme)
+                    {
+                        return ispravno;
+                    }
+                    
+                }
+                korisnik korisnik = new korisnik()
+                {
+                    ime = ime,
+                    prezime = prezime,
+                    korisnicko_ime = ime,
+                    lozinka = lozinka,
+                    email = email,
+                    adresa = adresa,
+                    telefon = telefon,
+                    uloga_id = 1
+                };
+                bp.korisniks.Add(korisnik);
+                bp.SaveChanges();
+                
+                ispravno = true;    
+            }
+            return ispravno;
+        }
+        public string prijaviKorisnika(string korIme, string lozinka)
+        {
+            string korRegistriran = "korime";
+            using (IneffableEntities bp = new IneffableEntities())
+            {
+                foreach (var korisnik in bp.korisniks)
+                {
+                    if (korisnik.korisnicko_ime == korIme)
+                    {
+                        if (korisnik.lozinka == lozinka)
+                        {
+                            korRegistriran = "";
+                        }
+                        else
+                        {
+                            korRegistriran = "lozinka";
+                        }
+                    }
+                }
+            }
+            return korRegistriran;
+        }
+    }
+}
--- Ineffable/PrijavaRegistracija/frmPrijava.cs
+++ Ineffable/PrijavaRegistracija/frmPrijava.cs
@@ -12,9 +12,75 @@ namespace PrijavaRegistracija
 {
     public partial class frmPrijava : Form
     {
-        public frmPrijava()
+        Form parent;
+        public frmPrijava(Form parent)
         {
             InitializeComponent();
+            this.parent = parent;
         }
+
+        int pomak;
+        int pomakX;
+        int pomakY;
+        private void frmPrijava_MouseDown(object sender, MouseEventArgs e)
+        {
+            pomak = 1;
+            pomakX = e.X;
+            pomakY = e.Y;
+        }
+
+        private void frmPrijava_MouseUp(object sender, MouseEventArgs e)
+        {
+            pomak = 0;
+        }
+
+        private void frmPrijava_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pomak == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - pomakX, MousePosition.Y - pomakY);
+            }
+        }
+
+        private void btnPrijava_Click(object sender, EventArgs e)
+        {
+            Autentifikator autentifikator = new Autentifikator();
+            string korIme = tbKorIme.Text;
+            string lozinka = tbLozinka.Text;
+            Cursor.Current = Cursors.WaitCursor;
+            if (korIme == "")
+            {
+                MessageBox.Show("Niste unjeli korisničko ime");
+            }
+            else if (lozinka == "")
+            {
+                MessageBox.Show("Niste unjeli lozinku");
+            }
+            else
+            {
+                string korisniknadjen = autentifikator.prijaviKorisnika(korIme, lozinka);
+                if (korisniknadjen == "")
+                {
+                    this.Close();
+                }
+                else if (korisniknadjen == "lozinka")
+                {
+                    MessageBox.Show("Kriva lozinka");
+                }
+                else
+                {
+                    MessageBox.Show("Krivo korisničko ime");
+                }
+            }        
+        }
+
+        private void Registracija_Click(object sender, EventArgs e)
+        {
+            frmRegistracija registracija = new frmRegistracija(parent);
+            registracija.MdiParent = parent;
+            registracija.WindowState = FormWindowState.Maximized;
+            registracija.Show();
+        }
+
     }
 }
