0de2a9e06cf79858d7c1b0f78bcab1d6496811e6
avulic <antevulic96@gmail.com>
2018-09-18 19:07:46 +0200

    added setup

--- Ineffable/PrijavaRegistracija/Properties/AssemblyInfo.cs
+++ Ineffable/PrijavaRegistracija/Properties/AssemblyInfo.cs
@@ -6,18 +6,18 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PrijavaRegistracija")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("prijaa")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ineffable")]
 [assembly: AssemblyProduct("PrijavaRegistracija")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  Ineffable 2018")]
+[assembly: AssemblyTrademark("Ineffable")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b29d18bb-d599-4b18-832e-2ff2d0e0fe64")]
